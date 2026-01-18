acdf23fb69876401a29a580f5e2462d2684f16a8
ScareCellar <98201877+ScareCellar@users.noreply.github.com>
2026-01-17 18:33:50 -0700

    implemeted Wandering, and fixed seek and flee

--- Assets/Agent/Scripts/AutonomousAgent.cs
+++ Assets/Agent/Scripts/AutonomousAgent.cs
@@ -1,19 +1,106 @@
+
 using UnityEngine;
+
 
 public class AutonomousAgent : AIAgent
 {
     [SerializeField] Movement movement;
-    [SerializeField] Perception perception;
+    [SerializeField] Perception seekPerception;
+    [SerializeField] Perception fleePerception;
+
+    [Header("Wander")]
+    [SerializeField] float wanderRadius = 1;
+    [SerializeField] float wanderDistance = 1;
+    [SerializeField] float wanderDisplacement = 1;
+    
+    float wanderAngle = 0.0f;
 
     void Start()
     {
-
+        wanderAngle = Random.Range(0,360);
     }
 
     void Update()
     {
-        movement.ApplyForce(Vector3.forward);
+        bool hasTarget = false;
+        if (seekPerception != null)
+        {
+            var gameObjects = seekPerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                hasTarget = true;
+                Vector3 force = Seek(gameObjects[0]);
+                movement.ApplyForce(force);
+            }
+        }
 
-        transform.position = Utilities.Wrap(transform.position, new Vector3 (-15,-15,-15), new Vector3(15,15,15));
+        if (fleePerception != null)
+        {
+            var gameObjects = fleePerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                hasTarget = true;
+                Vector3 force = Flee(gameObjects[0]);
+                movement.ApplyForce(force);
+            } 
+        }
+
+        if (!hasTarget)
+        {
+            Vector3 force = Wander();
+            //< apply force to movement >
+            movement.ApplyForce(force);
+        }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-15, -15, -15), new Vector3(15, 15, 15));
+        
+        if (movement.Velocity.sqrMagnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(transform.forward, Vector3.up);
+        }
+    }
+
+    Vector3 Seek(GameObject target)
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        Vector3 force = GetSteeringForce(direction);
+        return force;
+    }
+    Vector3 Flee(GameObject target)
+    {
+        Vector3 direction = transform.position - target.transform.position;
+        Vector3 force = GetSteeringForce(direction);
+        return force;
+    }
+
+    Vector3 GetSteeringForce(Vector3 direction)
+    {
+        Vector3 desired = direction.normalized * movement.maxSpeed;
+        Vector3 steer = desired - movement.Velocity;
+        return Vector3.ClampMagnitude(steer, movement.maxForce);
+    }
+    private Vector3 Wander()
+    {
+
+        // randomly adjust the wander angle within (+/-) displacement range 
+        wanderAngle += Random.Range(-wanderDisplacement, wanderDisplacement);
+        // calculate a point on the wander circle using the wander angle 
+
+        Quaternion rotation = Quaternion.AngleAxis(wanderAngle, Vector3.up);
+
+        Vector3 pointOnCircle = rotation * (Vector3.forward * wanderRadius);
+
+        // project the wander circle in front of the agent 
+
+        Vector3 circleCenter = movement.Direction * wanderDistance;
+
+        // steer toward the target point (circle center + point on circle) 
+
+        Vector3 force = GetSteeringForce(circleCenter + pointOnCircle);
+
+
+
+        return force;
+
     }
 }
--- Assets/Agent/Scripts/Movement.cs
+++ Assets/Agent/Scripts/Movement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public abstract class Movement : MonoBehaviour
 {
     public float maxSpeed = 1.0f;
-    public float minSpeed = 1.0f;
+    public float maxForce = 1.0f;
     public virtual Vector3 Velocity { get; set; }
     public virtual Vector3 Acceleration { get; set; }
     public virtual Vector3  Direction { get { return Velocity.normalized; } }
